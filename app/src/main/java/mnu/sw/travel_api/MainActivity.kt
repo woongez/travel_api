73c6b855636b1c3af2c980497a579acb828524ba
J  W <xmsxmsxpsxps@Naver.com>
2023-01-15 19:37:31 +0900

    API

---
+++ app/src/main/java/mnu/sw/travel_api/MainActivity.kt
@@ -0,0 +1,11 @@
+package mnu.sw.travel_api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
